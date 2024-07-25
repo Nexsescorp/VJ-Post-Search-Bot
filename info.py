98706c8cd47a02168bfd70083e888e69b6ec2ee0
Nexsescorp <ashusingh881062@gmail.com>
2024-07-25 15:39:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -9,8 +9,8 @@ from os import environ
 
 API_ID       = int(environ.get("API_ID", "28138322"))
 API_HASH     = environ.get("API_HASH", "286d256ab24622380e49ff4aed38de8b")
-BOT_TOKEN    = environ.get("BOT_TOKEN", "")
-DATABASE_URI = environ.get("DATABASE_URI", "")
+BOT_TOKEN    = environ.get("BOT_TOKEN", "7467672694:AAEBnPBDyYsI8ZqwcBjSWE1cF6RSGikzLEk")
+DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://mukeshsingh19819654220738:pkoGD9ZwOhvh4obz@cluster0.dhmtmz9.mongodb.net/?retryWrites=true&w=majority")
 LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002200775580"))
 ADMIN        = int(environ.get("ADMIN", "7291025420"))
 CHANNEL      = environ.get("CHANNEL", "@VJ_Botz")
